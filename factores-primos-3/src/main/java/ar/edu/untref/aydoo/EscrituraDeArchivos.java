d8eee1ac9316b4ec50728761d0216f561a95f889
unknown <r.pasquali2412@gmail.com>
2017-04-24 00:15:41 -0300

    Se crea clase EscrituraDeArchivos

---
+++ factores-primos-3/src/main/java/ar/edu/untref/aydoo/EscrituraDeArchivos.java
@@ -0,0 +1,45 @@
+package ar.edu.untref.aydoo;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class EscrituraDeArchivos {
+	
+	public static void main(String[] args){
+		
+		EscrituraDeArchivos escritor = new EscrituraDeArchivos();
+		escritor.escribirArchivo("salida.txt", "Hola como andas? " + "\r\n" + "Bien");
+	
+	}
+	
+	public void escribirArchivo(String directorio, String texto){
+		//File fichero = new File(sFichero);
+		/* 
+		if (fichero.exists()){
+			}
+		*/
+		FileWriter fichero = null;
+       // PrintWriter pw = null;
+        try
+        {
+            fichero = new FileWriter(directorio);
+            //pw = new PrintWriter(fichero);
+            //pw.println(texto);
+            fichero.write(texto);
+			
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+           try {
+           // Nuevamente aprovechamos el finally para 
+           // asegurarnos que se cierra el fichero.
+           if (null != fichero)
+              fichero.close();
+           } catch (Exception e2) {
+              e2.printStackTrace();
+           }
+        }
+	}
+
+}
