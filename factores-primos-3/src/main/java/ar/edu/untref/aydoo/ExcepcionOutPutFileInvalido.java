c51fc48383b44cb8ca822a9c1ad1c7d7fbac1bbd
RodrigoPasquali <r.pasquali2412@gmail.com>
2017-04-25 20:39:18 -0300

    Se modifica mensaje de ExcepcionOutPutFile

--- factores-primos-3/src/main/java/ar/edu/untref/aydoo/ExcepcionOutPutFileInvalido.java
+++ factores-primos-3/src/main/java/ar/edu/untref/aydoo/ExcepcionOutPutFileInvalido.java
@@ -2,7 +2,7 @@ package ar.edu.untref.aydoo;
 
 public class ExcepcionOutPutFileInvalido extends RuntimeException {
 
-    private final static String mensajeOutPutFileInvalido = " Archivo no aceptado. El archvio debe poseer extencion .txt";
+    private final static String mensajeOutPutFileInvalido = " Archivo no aceptado. El archvio debe poseer extension .txt";
     
     public ExcepcionOutPutFileInvalido(){
         super(mensajeOutPutFileInvalido);
