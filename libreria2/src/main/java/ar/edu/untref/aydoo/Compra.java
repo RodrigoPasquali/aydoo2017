47b1fce8d4ffda30a780a91f7228236d1b183bba
RodrigoPasquali <r.pasquali2412@gmail.com>
2017-05-19 05:30:17 -0300

    Se agrega clase Compra()

---
+++ libreria2/src/main/java/ar/edu/untref/aydoo/Compra.java
@@ -0,0 +1,12 @@
+package ar.edu.untref.aydoo;
+
+public class Compra {
+	private Mes mes;
+	private Producto producto;
+
+	public Compra(Producto productoComprado, Mes mesDeLaCompra){
+		this.producto = productoComprado;
+		this.mes = mesDeLaCompra;
+	}
+	
+}
