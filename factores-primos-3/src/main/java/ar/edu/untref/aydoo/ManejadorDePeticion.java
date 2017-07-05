0393fb30ae869f1a551ce66d9e95d41ce0b50588
RodrigoPasquali <r.pasquali2412@gmail.com>
2017-07-05 06:53:38 -0300

    Se borra lineas comentadas

--- factores-primos-3/src/main/java/ar/edu/untref/aydoo/ManejadorDePeticion.java
+++ factores-primos-3/src/main/java/ar/edu/untref/aydoo/ManejadorDePeticion.java
@@ -22,14 +22,6 @@ private String salidaSolicitadaIngresada;
     	ordenarFactoresPrimos();
     	darFormatoASalida();
     	ejecutarSalidaSolicitada();
-    	/*
-		if (this.outPutFile != null) {
-    		this.escritor = new SalidaEscrituraDeArchivos(this.outPutFile, this.salidaObtenida);
-    		this.escritor.escribirArchivo(this.outPutFile, this.salidaObtenida);
-    	} else {
-    		System.out.println(this.salidaObtenida);
-    	}
-    	*/
 	}
 	
 	private void obtenerArgumentos(String[] args) {
@@ -40,24 +32,24 @@ private String salidaSolicitadaIngresada;
     	this.salidaSolicitadaIngresada = obtenedor.getSalidaSolicitada(args);
 	}
 	
-	private void calcularFactoresPrimos(){
+	private void calcularFactoresPrimos() {
 		Calculadora calculadora = new Calculadora();
 		calculadora.calcularFactoresPrimos(this.numeroIngresado);
 		this.listaDeFactoresPrimos = calculadora.obtenerFactoresPrimos();
 	}
 	
-	private void ordenarFactoresPrimos(){
+	private void ordenarFactoresPrimos() {
 		Orden ordenador;
 		ordenador = new OrdenAscendente(this.ordenIngresado, this.listaDeFactoresPrimos);
 		this.listaDeFactoresPrimos = ordenador.aplicarOrden();
 	}
 	
-	private void darFormatoASalida(){
+	private void darFormatoASalida() {
 		Formato formateador = new FormatoPretty(this.formatoIngresado, this.listaDeFactoresPrimos, this.numeroIngresado);
 		this.salidaObtenida = formateador.aplicarFormato();
 	}
 	
-	private void ejecutarSalidaSolicitada() throws IOException{
+	private void ejecutarSalidaSolicitada() throws IOException {
 		Salida manejadorDeSalida = new SalidaConsola(this.salidaSolicitadaIngresada, this.salidaObtenida);
 		manejadorDeSalida.aplicarSalidaSolicitada();
 	}
