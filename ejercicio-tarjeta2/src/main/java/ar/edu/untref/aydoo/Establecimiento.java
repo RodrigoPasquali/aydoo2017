aa5a784ee8b69555ab81592ee3d09dfe82e6e1f8
RodrigoPasquali <r.pasquali2412@gmail.com>
2017-07-07 04:58:24 -0300

    Se agrega clase Establecimiento

---
+++ ejercicio-tarjeta2/src/main/java/ar/edu/untref/aydoo/Establecimiento.java
@@ -0,0 +1,25 @@
+package ar.edu.untref.aydoo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Establecimiento {
+	
+	private String nombreEstablecimiento;
+	private List<Sucursal> listaDeSucursales;
+	private List<Beneficio> listaDeBeneficios;
+
+	public Establecimiento(String nombre){
+		this.nombreEstablecimiento = nombre;
+		this.listaDeSucursales = new LinkedList<Sucursal>();
+		this.listaDeBeneficios = new LinkedList<Beneficio>();
+	}
+	
+	public void agregarSucursal(Sucursal sucursal){
+		this.listaDeSucursales.add(sucursal);
+	}
+	
+	public void agregarBeneficio(Beneficio beneficio){
+		this.listaDeBeneficios.add(beneficio);
+	}
+}
--- ejercicio-tarjeta2/src/main/java/ar/edu/untref/aydoo/Sucursal.java
+++ ejercicio-tarjeta2/src/main/java/ar/edu/untref/aydoo/Sucursal.java
@@ -1,5 +1,11 @@
 package ar.edu.untref.aydoo;
 
 public class Sucursal {
+	
+	private String nombreSucursal;
+
+	public Sucursal(String nombre){
+		this.nombreSucursal = nombre;
+	}
 
 }
