fa3dea3760a8b196be8302e83c7af02bbdb82c5e
RodrigoPasquali <r.pasquali2412@gmail.com>
2017-07-07 04:39:20 -0300

    Se agrega TestCompra con un test funcionando OK

---
+++ ejercicio-tarjeta2/src/test/java/ar/edu/untref/aydoo/TestCompra.java
@@ -0,0 +1,18 @@
+package ar.edu.untref.aydoo;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestCompra {
+	
+	@Test 
+	public void getProductoCompradoDeberiaDevolverHelado() {
+		Producto helado = new Producto(100, null);
+		Compra compra = new Compra(helado);
+		Producto valorEsperado = helado;
+		
+		Producto valorObtenido = compra.getProductoComprado();
+		
+		assertEquals(valorEsperado, valorObtenido);
+	}
+}
