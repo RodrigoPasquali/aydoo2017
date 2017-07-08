32260a5f6c36b51f24b31b0f294e8c1ec78933b9
RodrigoPasquali <r.pasquali2412@gmail.com>
2017-07-08 05:17:18 -0300

    En clase Club se agregan metodos agregarClinte() y agregarEstablecimiento(

--- ejercicio-tarjeta2/src/main/java/ar/edu/untref/aydoo/Club.java
+++ ejercicio-tarjeta2/src/main/java/ar/edu/untref/aydoo/Club.java
@@ -1,5 +1,23 @@
 package ar.edu.untref.aydoo;
 
+import java.util.LinkedList;
+
 public class Club {
+	
+	private LinkedList<Cliente> listaClientes;
+	private LinkedList<Establecimiento> listaEstablecimiento;
+
+	public Club() {
+		this.listaEstablecimiento = new LinkedList<Establecimiento>();
+		this.listaClientes = new LinkedList<Cliente>();
+	}
+	
+	public void agregarEstablecimiento(Establecimiento establciemiento) {
+		this.listaEstablecimiento.add(establciemiento);
+	}
+	
+	public void agregarCliente(Cliente cliente) {
+		this.listaClientes.add(cliente);
+	}
 
 }
